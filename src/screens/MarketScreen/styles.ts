9392f099a5017b398508dabc25eb9d0c3661de02
Di Lu <dilu@andrew.cmu.edu>
2021-08-12 10:28:51 -0700

    finish coin UI

---
+++ src/screens/MarketScreen/styles.ts
@@ -0,0 +1,34 @@
+import  {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    root:{
+        flex:1,
+        padding: 0,
+        alignItems: "center",
+        backgroundColor: '#ffffff',
+    },
+    image:{
+        marginTop: "30%",
+        height: 175,
+        aspectRatio: 1,
+    },
+    label:{
+        fontSize: 18,
+        fontWeight:"bold",
+        color: '#626161'
+
+    },
+    balance:{
+        fontSize: 36,
+        fontWeight:"bold",
+        color: '#371001'
+
+    },
+    balanceContainer:{
+        marginVertical:20,
+        width:"100%"
+    },
+
+});
+
+export default styles;
--- src/screens/PortforlioScreen/index.tsx
+++ src/screens/PortforlioScreen/index.tsx
@@ -32,27 +32,63 @@ const AssetCoins = [
         "amount" : 371.99,
         "valueUSD" : 371.99,
     },
+    {
+        "id" : 1,
+        "name" : "Staked",
+        "image": stakedImage,
+        "symbol" : "USD",
+        "amount" : 11.29,
+        "valueUSD" : 11.29,
+    },
+    {
+        "id" : 2,
+        "name" : "Available",
+        "image": availableImage,
+        "symbol" : "USD",
+        "amount" : 129.01,
+        "valueUSD" : 129.01,
+    },
+    {
+        "id" : 2,
+        "name" : "Pending",
+        "image": pendingImage,
+        "symbol" : "USD",
+        "amount" : 371.99,
+        "valueUSD" : 371.99,
+    },
 ]
 
 const PortforlioScreen=(props)=>{
     return(
         <View style={styles.root}>
-            <Image style={styles.image} source={image}/>
+            <>
+                <Image style={styles.image} source={image}/>
 
-            <View style={styles.balanceContainer}>
-                <Text style={styles.label}>Your Assets and Balance:</Text>
-                <Text style={styles.balance}>$511.29</Text>
-            </View>
+                <View style={styles.balanceContainer}>
+                    <Text style={styles.label}>Your Assets and Balance:</Text>
+                    <Text style={styles.balance}>$511.29</Text>
+                </View>
+            </>
             
-            <FlatList 
-                data = {AssetCoins} 
+            <FlatList
+                // style={{width:'100%'}}
+                data = {AssetCoins}
                 renderItem={({item}) => <AssetCoin AssetCoin={item} />}
-                      
-            
+                showsHorizontalScrollIndicator={false}
+                ListHeaderComponentStyle = {{alignItems:'center'}}
+                // ListHeaderComponent = {() => (
+                //     <>
+                //         <Image style={styles.image} source={image}/>
+                //
+                //         <View style={styles.balanceContainer}>
+                //             <Text style={styles.label}>Your Assets and Balance:</Text>
+                //             <Text style={styles.balance}>$511.29</Text>
+                //         </View>
+                //     </>
+                // )}
             />
-
         </View>
     );
-}
+};
 
 export default PortforlioScreen;
