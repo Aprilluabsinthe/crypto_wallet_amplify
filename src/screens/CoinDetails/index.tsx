25db7b1d9e64bdf93781987eda543dd68857dd39
Di Lu <dilu@andrew.cmu.edu>
2021-08-12 21:51:21 -0700

    add coin datails page

---
+++ src/screens/CoinDetails/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View , Text, Image} from'react-native';
+import styles from './styles';
+const image = require( '../../../assets/images/tezoscoin.png')
+
+const CoinDetailsScreen=(props)=>{
+    return(
+        <View style={styles.root}>
+            <Image style={styles.image} source={image}/>
+            <Text style={styles.header1} >Welcome To Tezos Wallet!</Text>
+            <Text style={styles.header2} >Invest, Stake, Transfer and so on. Every thing you may need to play with Tezos</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
---
+++ src/screens/CoinDetails/styles.ts
@@ -0,0 +1,29 @@
+import  {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    root:{
+        flex:1,
+        padding: 20,
+        alignItems: "center",
+        backgroundColor: '#ffffff',
+    },
+    image:{
+        marginTop: "30%",
+        height: '30%',
+        aspectRatio: 1,
+    },
+    header1:{
+        fontSize: 25,
+        fontWeight:"bold",
+        marginTop: 50,
+        marginBottom: 20,
+    },
+    header2:{
+        fontSize: 18,
+        textAlign: "center",
+        color:'#414141'
+    }
+
+});
+
+export default styles;
