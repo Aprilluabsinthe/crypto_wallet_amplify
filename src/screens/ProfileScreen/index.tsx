7096ce2aa71f6d0d26df0b126510dec5dd242612
Di Lu <dilu@andrew.cmu.edu>
2021-08-12 20:54:19 -0700

    add coin details

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import{View,Text, Image, FlatList}from'react-native';
+import React, {useState} from 'react';
+import {View, Text, Image, FlatList, Pressable} from 'react-native';
 import styles from './styles'
 const image = require("../../../assets/images/Saly-16.png");
+const avatar = require("../../../assets/images/avatar.png");
 
 const ProfileScreen=(props)=>{
+    const [user , setUser] = useState({
+        id: '1',
+        name: "Di Lu",
+        email: "dilu@andrew.cmu.edu",
+        image: avatar,
+        netWorth: 51129.22
+    })
+
+    const signOut = () =>{
+        console.warn("Sign Out");
+    }
+
     return(
         <View style={styles.root}>
             <Image style={styles.image} source={image} />
-            <Text>It's a ProfileScreen Component</Text>
+            <View style={styles.userContainer}>
+                <View style={styles.left}>
+                    <Image style={styles.avatar} source = {user.image}/>
+                    <View>
+                        <Text style={styles.name}>{user.name}</Text>
+                        <Text style={styles.email}>{user.email}</Text>
+                    </View>
+                </View>
+
+
+                <View style={{alignItems:'flex-end'}}>
+                    <Text style={styles.value}>${user.netWorth}</Text>
+                </View>
+            </View>
+            <Pressable  style={{marginTop:'auto'}} onPress={signOut}>
+                <Text style={styles.signOut}>Sign Out</Text>
+            </Pressable>
         </View>
     );
 }
--- src/screens/ProfileScreen/styles.ts
+++ src/screens/ProfileScreen/styles.ts
@@ -8,14 +8,53 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffffff',
     },
     image:{
+        height: 175,
+        resizeMode:"contain",
+        backgroundColor:"white",
         marginTop: "20%",
-        height: '25%',
-        aspectRatio: 1,
     },
-    label:{
-        fontSize: 18,
+    userContainer:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        backgroundColor: '#f8f7f7',
+        width:'95%',
+        height:80,
+        margin:10,
+        marginVertical:10,
+    },
+    avatar:{
+        height: 75,
+        width: 75,
+        backgroundColor:"white",
+        borderRadius: 50,
+        marginRight: 10,
+    },
+    left:{
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    right:{
+        flexDirection:"row",
+
+    },
+    name:{
+        fontSize:20,
         fontWeight:"bold",
-        color: '#626161'
+        marginBottom: 5,
+    },
+    email:{
+        fontSize:18,
+        marginBottom: 5,
+    },
+    symbol:{
+        textAlign: "center",
+        color:'#6d6c6c'
+    },
+    value:{
+        fontSize:18,
+        fontWeight:"bold",
+        marginTop: 20,
+        marginBottom: 5,
     },
     balance:{
         fontSize: 36,
@@ -25,6 +64,13 @@ const styles = StyleSheet.create({
     balanceContainer:{
         marginVertical:20,
         width:"90%"
+    },
+    signOut:{
+        fontWeight:"bold",
+        fontSize:20,
+        marginVertical:"20%",
+        backgroundColor:'#91b7ef',
+        color: '#011a40'
     },
 
 });
